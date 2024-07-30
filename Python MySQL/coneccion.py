accffcc388c5018781f6db2bb5b82ed5d7423596
Prestes28 <144856856+Prestes28@users.noreply.github.com>
2024-07-30 11:34:20 -0300

    Delete Python MySQL directory

--- Python MySQL/coneccion.py
+++
@@ -1,6 +0,0 @@
-#pip install mysql-connector-python
-import mysql.connector
-conexion= mysql.connector.connect(user="root", password="", host = "localhost", database="taller_de_desarrollo",port="3306")
-
-print(conexion)
-
